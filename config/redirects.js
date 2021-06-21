14365c43fa8a28b5cd96d288649a29b284159f7d
Josh Cunningham <josh@joshcanhelp.com>
2021-06-21 11:50:46 -0700

    fix: remove instagram redirect

--- config/redirects.js
+++ config/redirects.js
@@ -577,7 +577,6 @@ module.exports = [
     from: [
       '/facebook-clientid',
       '/connections/social/facebook',
-      '/connections/social/instagram',
       '/instagram-clientid'
     ],
     to: 'https://marketplace.auth0.com/integrations/facebook-social-connection'
